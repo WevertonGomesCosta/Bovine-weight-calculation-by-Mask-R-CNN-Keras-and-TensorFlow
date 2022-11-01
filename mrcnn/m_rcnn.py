74ac8a3dc1fcc9661c923d92fc9fb0678fa7b8aa
Weverton Gomes da Costa <95870682+WevertonGomesCosta@users.noreply.github.com>
2022-11-01 15:07:49 -0300

    Update m_rcnn.py

--- mrcnn/m_rcnn.py
+++ mrcnn/m_rcnn.py
@@ -75,7 +75,7 @@ class CustomConfig(Config):
     TRAIN_ROIS_PER_IMAGE = 32
 
     # Use a small epoch since the data is simple
-    STEPS_PER_EPOCH = 500
+    STEPS_PER_EPOCH = 100
 
     # use small validation steps since the epoch is small
     VALIDATION_STEPS = 5
