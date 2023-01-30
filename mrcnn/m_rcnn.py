f1b023e2755fb00b04bcd02b0e69074d92234b1d
Weverton Gomes da Costa <95870682+WevertonGomesCosta@users.noreply.github.com>
2023-01-30 10:07:00 -0300

    Update m_rcnn.py

--- mrcnn/m_rcnn.py
+++ mrcnn/m_rcnn.py
@@ -59,7 +59,7 @@ class CustomConfig(Config):
     IMAGES_PER_GPU = 4
 
     # Number of classes
-    NUM_CLASSES = 1
+    NUM_CLASSES = 1 + 1
 
     # Use small images for faster training. Set the limits of the small side
     # the large side, and that determines the image shape.
