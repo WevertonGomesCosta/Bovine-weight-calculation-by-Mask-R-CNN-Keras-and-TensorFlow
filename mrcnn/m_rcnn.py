102e6db4ec0d17b715436893166c6f202b560cf0
WevertonGomesCosta <weverton.costa@ufv.br>
2023-02-15 16:52:44 -0300

    Update identation CustomConfig

--- mrcnn/m_rcnn.py
+++ mrcnn/m_rcnn.py
@@ -44,37 +44,39 @@ class CustomConfig(Config):
         # Number of classes
         NUM_CLASSES = 1 + num_classes
 
-    """Configuration for training on the toy shapes dataset.
-    Derives from the base Config class and overrides values specific
-    to the toy shapes dataset.
-    """
-    # Give the configuration a recognizable name
-    NAME = "object"
+        """Configuration for training on the toy shapes dataset.
+        Derives from the base Config class and overrides values specific
+        to the toy shapes dataset.
+        """
+        # Give the configuration a recognizable name
+        NAME = "object"
 
-    # Train on 1 GPU and 8 images per GPU. We can put multiple images on each
-    # GPU because the images are small. Batch size is 8 (GPUs * images/GPU).
-    GPU_COUNT = 1
-    IMAGES_PER_GPU = 4
+        # Train on 1 GPU and 8 images per GPU. We can put multiple images on each
+        # GPU because the images are small. Batch size is 8 (GPUs * images/GPU).
+        GPU_COUNT = 1
+        IMAGES_PER_GPU = 4
 
-    # Use small images for faster training. Set the limits of the small side
-    # the large side, and that determines the image shape.
-    IMAGE_MIN_DIM = 512
-    IMAGE_MAX_DIM = 512
+        # Use small images for faster training. Set the limits of the small side
+        # the large side, and that determines the image shape.
+        IMAGE_MIN_DIM = 512
+        IMAGE_MAX_DIM = 512
 
-    # Use smaller anchors because our image and objects are small
-    # RPN_ANCHOR_SCALES = (8, 16, 32, 64, 128)  # anchor side in pixels
+        # Use smaller anchors because our image and objects are small
+        # RPN_ANCHOR_SCALES = (8, 16, 32, 64, 128)  # anchor side in pixels
 
-    # Reduce training ROIs per image because the images are small and have
-    # few objects. Aim to allow ROI sampling to pick 33% positive ROIs.
-    # TRAIN_ROIS_PER_IMAGE = 32
+        # Reduce training ROIs per image because the images are small and have
+        # few objects. Aim to allow ROI sampling to pick 33% positive ROIs.
+        # TRAIN_ROIS_PER_IMAGE = 32
 
-    # Use a small epoch since the data is simple
-    STEPS_PER_EPOCH = 100
+        # Use a small epoch since the data is simple
+        STEPS_PER_EPOCH = 500
 
-    # use small validation steps since the epoch is small
-    VALIDATION_STEPS = 10
+        # use small validation steps since the epoch is small
+        VALIDATION_STEPS = 5
 
-    DETECTION_MIN_CONFIDENCE = 0.9
+        # Minimum probability value to accept a detected instance
+        # ROIs below this threshold are skipped
+        DETECTION_MIN_CONFIDENCE = 0.9
 
 """
 NOTEBOOK PREFERENCES
