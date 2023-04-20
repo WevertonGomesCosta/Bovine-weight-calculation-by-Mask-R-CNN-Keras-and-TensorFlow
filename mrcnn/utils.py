18e7753c7aa7a20d6b13b53c51ed38b110930202
Weverton Gomes da Costa <95870682+WevertonGomesCosta@users.noreply.github.com>
2023-04-20 14:00:39 -0300

    Update utils.py

--- mrcnn/utils.py
+++ mrcnn/utils.py
@@ -893,8 +893,17 @@ def resize(image, output_shape, order=1, mode='constant', cval=0, clip=True,
     of skimage. This solves the problem by using different parameters per
     version. And it provides a central place to control resizing defaults.
     """
-    
-    return skimage.transform.resize(
-        image, output_shape,
-        order=order, mode=mode, cval=cval, clip=clip,
-        preserve_range=preserve_range)
+    if LooseVersion(skimage.__version__) >= LooseVersion("0.14"):
+        # New in 0.14: anti_aliasing. Default it to False for backward
+        # compatibility with skimage 0.13.
+        return skimage.transform.resize(
+            image, output_shape,
+            order=order, mode=mode, cval=cval, clip=clip,
+            preserve_range=preserve_range, anti_aliasing=anti_aliasing,
+            anti_aliasing_sigma=anti_aliasing_sigma)
+    else:
+        return skimage.transform.resize(
+            image, output_shape,
+            order=order, mode=mode, cval=cval, clip=clip,
+            preserve_range=preserve_range)
+
